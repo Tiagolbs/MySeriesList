0b1ea4506f0089ada7bfd53c4a3f3a5ffea1ba7a
Tiago Seben <tiagolbs31@gmail.com>
2020-05-03 20:34:30 -0300

    Create default.blade.php
    sweetalert

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,32 @@
+@extends('adminlte::page')
+
+@section('plugins.Sweetalert2', true)
+
+@section('js')
+    <script>
+        function ConfirmaExclusao(id) {
+            swal.fire({
+                title: 'Are you sure?', text: "You can add that series again later",
+                type: 'warning', showCancelButton: true, confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33', confirmButtonText: 'Delete',
+                cancelButtonText: 'Nope', closeOnConfirm: false,
+            }).then(function(isConfirm){
+                if (isConfirm.value){
+                    $.get('/'+ @yield('table-delete') +'/'+id+'/destroy', function(data){
+                        swal.fire(
+                            'Deleted!',
+                            'You did it bro!',
+                            'success'
+                        ).then(function(){
+                            window.location.reload();
+                        });
+                    });
+                }
+            })
+        }
+    </script>
+@stop
+
+@section('table-delete')
+"list_series"
+@endsection
