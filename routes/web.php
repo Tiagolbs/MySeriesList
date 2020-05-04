de27d39f85d9634b23cb6c4f466b2879ec87a2de
Tiago Seben <tiagolbs31@gmail.com>
2020-05-04 08:32:54 -0300

    Update web.php
    Rotas atualizadas

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,16 @@ Route::get('/', function () {
 
 
 Route::group(['prefix'=>'serieslist', 'where'=>['id'=>'[0-9]+']], function() {
-    Route::get('',                  ['as'=>'serieslist',           'uses'=>'seriesListController@index']);
-    Route::get('create',            ['as'=>'serieslist.create',    'uses'=>'seriesListController@create']);
-    Route::get('{id}/destroy',      ['as'=>'serieslist.destroy',   'uses'=>'seriesListController@destroy']);
-    Route::get('{id}/edit',         ['as'=>'serieslist.edit',      'uses'=>'seriesListController@edit']);
-    Route::put('{id}/update',       ['as'=>'serieslist.update',    'uses'=>'seriesListController@update']);
-    Route::post('store',             ['as'=>'serieslist.store',     'uses'=>'seriesListController@store']);
-    Route::get('noDate',                  ['as'=>'serieslist.noDate',           'uses'=>'seriesListController@noDate']);
+    Route::get('',                  ['as'=>'serieslist',                'uses'=>'seriesListController@index']);
+    Route::get('create',            ['as'=>'serieslist.create',         'uses'=>'seriesListController@create']);
+    Route::get('{id}/destroy',      ['as'=>'serieslist.destroy',        'uses'=>'seriesListController@destroy']);
+    Route::get('{id}/edit',         ['as'=>'serieslist.edit',           'uses'=>'seriesListController@edit']);
+    Route::put('{id}/update',       ['as'=>'serieslist.update',         'uses'=>'seriesListController@update']);
+    Route::post('store',            ['as'=>'serieslist.store',          'uses'=>'seriesListController@store']);
+    Route::get('{id}/addEp',        ['as'=>'serieslist.addEp',          'uses'=>'seriesListController@addEp']);
+    Route::get('{id}/removeEp',     ['as'=>'serieslist.removeEp',       'uses'=>'seriesListController@removeEp']);
+    Route::get('completed',         ['as'=>'serieslist.onlyCompleted',  'uses'=>'seriesListController@onlyCompleted']);
+    Route::get('watching',         ['as'=>'serieslist.onlyWatching',  'uses'=>'seriesListController@onlyWatching']);
 });
 
 Route::get('series', 'seriesController@index');
