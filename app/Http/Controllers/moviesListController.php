92599cb37f8c853daba18ce8f76f662232ffb468
Tiago Seben <tiagolbs31@gmail.com>
2020-05-04 15:16:20 -0300

    Update moviesListController.php

--- app/Http/Controllers/moviesListController.php
+++ app/Http/Controllers/moviesListController.php
@@ -9,7 +9,10 @@ use App\Serie;
 class moviesListController extends Controller
 {
     public function index() { 
-        $movies = DB::table('list_movies')->join('movies', 'movies.idMovie', '=', 'list_movies.idMovie')->where('list_movies.idUser', auth()->user()->id)->select('movies.poster as poster')->get();
+        $movies = DB::table('list_movies')->join('movies', 'movies.idMovie', '=', 'list_movies.idMovie')
+                                          ->where('list_movies.idUser', auth()->user()->id)
+                                          ->select('movies.poster as poster, movies.nome as nomeMovie, movies.status as status, movies_list.created_at as createDate, movies_list.updated_at as lastUpdate')
+                                          ->orderBy('status', 'desc')->paginate(10);
         return view('movieslist.index', ['movies' => $movies]);
     }
 }
