efa9a97bece216aa6ee2fe51c4c533cd54f4cc01
Tiago Seben <tiagolbs31@gmail.com>
2020-05-04 08:32:38 -0300

    Update seriesListController.php
    Funções para filtragem da lista e botões para controle de eps

--- app/Http/Controllers/seriesListController.php
+++ app/Http/Controllers/seriesListController.php
@@ -8,7 +8,10 @@ use App\listSerie;
 class seriesListController extends Controller
 {
     public function index() {
-        $series = listSerie::join('series', 'series.idSerie', '=', 'list_series.idSerie')->where('list_series.idUser', auth()->user()->id)->select('list_series.idListSeries as id', 'series.poster as poster', 'series.nomeSerie as nomeSerie', 'list_series.epsAssistidos as epsAssistidos', 'list_series.epsTotais as epsTotais', 'list_series.status as status', 'list_series.temporada as temporada')->orderBy('status', 'desc')->paginate(10);
+        $series = listSerie::join('series', 'series.idSerie', '=', 'list_series.idSerie')
+                            ->where('list_series.idUser', auth()->user()->id)
+                            ->select('list_series.idListSeries as id', 'series.poster as poster', 'series.nomeSerie as nomeSerie', 'list_series.epsAssistidos as epsAssistidos', 'list_series.epsTotais as epsTotais', 'list_series.status as status', 'list_series.temporada as temporada', 'list_series.created_at as createdate', 'list_series.updated_at as lastupdate')
+                            ->orderBy('status', 'desc')->paginate(10);
             return view('serieslist.index', ['series' => $series]);
     }
 
@@ -36,6 +39,46 @@ class seriesListController extends Controller
     public function update($id){
         listSerie::find($id)->update($request->all());
         return redirect()->route('serieslist');
+    }
+
+    public function addEp($id){
+        listSerie::find($id)->increment('epsAssistidos', 1);
+        $serie = listSerie::find($id);
+        if($serie->epsAssistidos == $serie->epsTotais){
+            $status = listSerie::find($id);
+            $status->status = "Completed";
+            $status->save();
+        }
+        return redirect()->route('serieslist');
+    }
+
+    public function removeEp($id){
+        listSerie::find($id)->decrement('epsAssistidos', 1);
+        $serie = listSerie::find($id);
+        if($serie->epsAssistidos < $serie->epsTotais){
+            $status = listSerie::find($id);
+            $status->status = "Watching";
+            $status->save();
+        }
+        return redirect()->route('serieslist');
+    }
+
+    public function onlyCompleted(){
+        $series = listSerie::join('series', 'series.idSerie', '=', 'list_series.idSerie')
+                            ->where('list_series.idUser', auth()->user()->id)
+                            ->where('list_series.status', 'Completed')
+                            ->select('list_series.idListSeries as id', 'series.poster as poster', 'series.nomeSerie as nomeSerie', 'list_series.epsAssistidos as epsAssistidos', 'list_series.epsTotais as epsTotais', 'list_series.status as status', 'list_series.temporada as temporada', 'list_series.created_at as createdate', 'list_series.updated_at as lastupdate')
+                            ->orderBy('status', 'desc')->paginate(10);
+        return view('serieslist.index', ['series' => $series]);
+    }
+
+    public function onlyWatching(){
+        $series = listSerie::join('series', 'series.idSerie', '=', 'list_series.idSerie')
+                            ->where('list_series.idUser', auth()->user()->id)
+                            ->where('list_series.status', 'Watching')
+                            ->select('list_series.idListSeries as id', 'series.poster as poster', 'series.nomeSerie as nomeSerie', 'list_series.epsAssistidos as epsAssistidos', 'list_series.epsTotais as epsTotais', 'list_series.status as status', 'list_series.temporada as temporada', 'list_series.created_at as createdate', 'list_series.updated_at as lastupdate')
+                            ->orderBy('status', 'desc')->paginate(10);
+        return view('serieslist.index', ['series' => $series]);
     }
 
 }
