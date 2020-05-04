d9596b8a6faedf3307dffd3c07002f6c8dd97bc5
Tiago Seben <tiagolbs31@gmail.com>
2020-05-04 15:16:36 -0300

    Update seriesListController.php
    Plan to watch adicionada

--- app/Http/Controllers/seriesListController.php
+++ app/Http/Controllers/seriesListController.php
@@ -48,6 +48,10 @@ class seriesListController extends Controller
             $status = listSerie::find($id);
             $status->status = "Completed";
             $status->save();
+        }else if($serie->status == 'Plan to Watch'){
+            $status = listSerie::find($id);
+            $status->status = "Watching";
+            $status->save();
         }
         return redirect()->route('serieslist');
     }
@@ -55,9 +59,13 @@ class seriesListController extends Controller
     public function removeEp($id){
         listSerie::find($id)->decrement('epsAssistidos', 1);
         $serie = listSerie::find($id);
-        if($serie->epsAssistidos < $serie->epsTotais){
+        if($serie->epsAssistidos < $serie->epsTotais and $serie->epsAssistidos > 0){
             $status = listSerie::find($id);
             $status->status = "Watching";
+            $status->save();
+        }else if($serie->epsAssistidos == 0){
+            $status = listSerie::find($id);
+            $status->status = "Plan to Watch";
             $status->save();
         }
         return redirect()->route('serieslist');
@@ -68,7 +76,7 @@ class seriesListController extends Controller
                             ->where('list_series.idUser', auth()->user()->id)
                             ->where('list_series.status', 'Completed')
                             ->select('list_series.idListSeries as id', 'series.poster as poster', 'series.nomeSerie as nomeSerie', 'list_series.epsAssistidos as epsAssistidos', 'list_series.epsTotais as epsTotais', 'list_series.status as status', 'list_series.temporada as temporada', 'list_series.created_at as createdate', 'list_series.updated_at as lastupdate')
-                            ->orderBy('status', 'desc')->paginate(10);
+                            ->orderBy('nomeSerie')->paginate(10);
         return view('serieslist.index', ['series' => $series]);
     }
 
@@ -77,7 +85,16 @@ class seriesListController extends Controller
                             ->where('list_series.idUser', auth()->user()->id)
                             ->where('list_series.status', 'Watching')
                             ->select('list_series.idListSeries as id', 'series.poster as poster', 'series.nomeSerie as nomeSerie', 'list_series.epsAssistidos as epsAssistidos', 'list_series.epsTotais as epsTotais', 'list_series.status as status', 'list_series.temporada as temporada', 'list_series.created_at as createdate', 'list_series.updated_at as lastupdate')
-                            ->orderBy('status', 'desc')->paginate(10);
+                            ->orderBy('nomeSerie')->paginate(10);
+        return view('serieslist.index', ['series' => $series]);
+    }
+
+    public function onlyPlanToWatch(){
+        $series = listSerie::join('series', 'series.idSerie', '=', 'list_series.idSerie')
+                            ->where('list_series.idUser', auth()->user()->id)
+                            ->where('list_series.status', 'Plan to Watch')
+                            ->select('list_series.idListSeries as id', 'series.poster as poster', 'series.nomeSerie as nomeSerie', 'list_series.epsAssistidos as epsAssistidos', 'list_series.epsTotais as epsTotais', 'list_series.status as status', 'list_series.temporada as temporada', 'list_series.created_at as createdate', 'list_series.updated_at as lastupdate')
+                            ->orderBy('nomeSerie')->paginate(10);
         return view('serieslist.index', ['series' => $series]);
     }
 
