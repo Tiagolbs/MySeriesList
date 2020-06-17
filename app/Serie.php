26a7c0d4b3b4098a96c11a0cc59735ef9776b29a
Tiago Seben <tiagolbs31@gmail.com>
2020-06-17 20:07:38 -0300

    Update Serie.php

--- app/Serie.php
+++ app/Serie.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Serie extends Model
 {
     protected $table = "series";
-    protected $fillable = ['idSerieImdb', 'nomeSerie', 'numTemps', 'descricao', 'generoSerie', 'notaSerie', 'poster'];
+    protected $fillable = ['idSerieImdb', 'nomeSerie', 'numTemps', 'descricao', 'generoSerie', 'notaSerie', 'poster', 'epTime'];
+    protected $primaryKey = 'idSerie';
 }
