d396357aec8a77b54749f4e92c7fe92a8a98f08e
Tiago Seben <tiagolbs31@gmail.com>
2020-06-18 04:13:44 -0300

    Create 2020_06_18_034849_create_friends_table.php

---
+++ database/migrations/2020_06_18_034849_create_friends_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('idUser');
+            $table->unsignedBigInteger('idFriend');
+            $table->string('name', 1000);
+            $table->foreign('idUser')->references('id')->on('users');
+            $table->foreign('idFriend')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
