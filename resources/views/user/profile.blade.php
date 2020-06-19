e3fd42fde637bdd81f5381bb3f7d424d77fd1c86
Tiago Seben <tiagolbs31@gmail.com>
2020-06-19 05:16:47 -0300

    Update profile.blade.php
    Adicionado status pie chart, utilizando google chart api

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,31 +1,96 @@
 @extends('layouts.default')
 
 @section('content')
-        <center><h1 style="font-family:fantasy">Status {{$user->name}}</h1><center>
+
+    {{--https://developers.google.com/chart/interactive/docs/gallery/piechart--}}
+    <html>
+        <head>
+            <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+            <script type="text/javascript">
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+
+                var data = google.visualization.arrayToDataTable([
+                ['Task', 'Hours per Day'],
+                ['Watching',     <?php echo $countWatching ?>],
+                ['Completed',      <?php echo $countCompleted ?>],
+                ['Plan to Watch',  <?php echo $countPtoW ?>],
+                ]);
+
+                var options = {
+                backgroundColor: '#F4F6F9',
+                is3D: true,
+                chartArea: {width:'100%',height:'100%'},
+                fontSize: 16
+                };
+
+                var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+                chart.draw(data, options);
+            }
+            </script>
+        </head>
+    </html>        
+
+
+
+        <center><h1 style="font-family:fantasy">Profile {{$user->name}}</h1><center>
         
-        <?php if(auth()->user()->name != $user->name) : ?>
+        <?php if(auth()->user()->name != $user->name and $test == NULL):?>
                 <a href="{{ route('user.addFriend', ['name'=>$user->name])}}" class="btn btn-outline-secondary btn-sm"><b>ADD FRIEND</b></a>
         <?php endif ?>
+        
+        <br>
+        
+        <div style="width:100%;">
+        
+            <div style="width:30%; float: left;">
+            <table class="table table-strip table-bordered table-hover">
+                <thead>
+                    <th>how much time did you spend in your life?</th>
+                </thead>
+                        <tr>
+                            <td >           
+                            Total Watched Episodes: {{$user->lifeEps}}</br>
+                            Total Watched Time (minutes): {{$user->lifeTime}}</br>
+                            Total Watched Time (hours): {{round($user->lifeTime/60,2)}}</br>
+                            Total Watched Time (days): {{round($user->lifeTime/60/24,2)}}
+                            </td>
+                        </tr>
+            </table>
+            </div>
 
-        Total Watched Episodes: {{$user->lifeEps}}</br>
-        Total Watched Time (minutes): {{$user->lifeTime}}</br>
-        Total Watched Time (hours): {{round($user->lifeTime/60,2)}}</br>
-        Total Watched Time (days): {{round($user->lifeTime/60/24,2)}}
+            <div style="float: left; width: 40%">
+            <table class="table table-strip table-bordered table-hover">
+                <thead>
+                    <th>Series Status Chart</th>
+                </thead>
+                        <tr>
+                            <td>           
+                                <div id="piechart" style="width: 100%; height: 300px; float:left;"></div>
+                            </td>
+                        </tr>
+            </table>
+            </div>
 
-
-        <?php if(auth()->user()->name == $user->name) : ?>
-        <table class="table table-strip table-bordered table-hover">
-            <thead>
-                <th>Friends</th>
-            </thead>
-                  
-        @foreach($friendsList as $friend)
-                    <tr>
-                        <td  style="text-align:center; vertical-align:middle"><a href="{{ route('user.profile', ['name'=>$friend->name])}}"><b>{{$friend->name}}</b></a>
-                        <a href="{{ route('user.deleteFriend', ['id'=>$friend->id])}}" class = "btn-sm btn-danger"><b>DELETE</b></a>
-                        </td>
-                    </tr>
-        @endforeach
-        </table>
-        <?php endif ?>
+            <div style="width:30%; float: left">
+            <?php if(auth()->user()->name == $user->name) : ?>
+            <table class="table table-strip table-bordered table-hover">
+                <thead>
+                    <th>Friends <a style='font-size:14px; float: right'href="{{ route('user.searchFriend')}}" class = "btn-sm btn-info"><b>Search Friend </b><i class="fa fa-search"></i></a></th>
+                </thead>
+                    
+            @foreach($friendsList as $friend)
+                        <tr>
+                            <td  style="text-align:center; vertical-align:middle"><a style="color: black" href="{{ route('user.profile', ['name'=>$friend->name])}}"><b>{{$friend->name}}</b></a>
+                            <a style="float:right" href="{{ route('user.deleteFriend', ['id'=>$friend->id])}}" class = "btn-sm btn-danger"><b>DELETE</b></a>
+                            </td>
+                        </tr>
+            @endforeach
+            </table>
+            <?php endif ?>
+            </div>
+        <div>
 @stop
