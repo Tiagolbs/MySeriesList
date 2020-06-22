756d45410955fa1e8d98aaaedc0395cae43afdbc
Tiago Seben <tiagolbs31@gmail.com>
2020-06-21 22:12:10 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         {!! Form::open(['name'=>'form_name', 'route'=>'home']) !!}
             <div class="sidebar-form" style="width:100%">
                 <div class="input-group" style="width:45%; margin:auto">
-                    <input type="text" name="desc_filtro" class="form-control" style="width:80% !important;" placeholder="Search tv show or movie">
+                    <input type="text" name="desc_filtro" class="form-control" style="width:80% !important;" placeholder="Search for tv show or movie">
                     <span class="input-group-btn">
                         <button type="submit" name="search" id="search-btn" class="btn btn-default"><i class="fa fa-search"></i></button>
                     </span>
@@ -15,13 +15,13 @@
         {!! Form::close() !!}
 
         <br>
-        <center><b>Popular Shows</b></center>
 
         <div style="float: right; width: 50%">
-        <table class="table table-strip table-bordered table-hover">
+        <table class="table table-strip table-bordered table-hover" style="background: #F4F6F9">
             <thead>
                 <th>Poster</th>
-                <th>Title</th>
+                <th>Movie Title </th>
+                <th>Score</th>
                 <th></th>
             </thead>
 
@@ -30,7 +30,8 @@
                     <tr>
                         <td style="width:5%"><img width = 95.4px height = 142.8px src="{{'https://image.tmdb.org/t/p/w500/'.$Movie['poster_path']}}" alt="poster"></td>
                         <td style="text-align:center; vertical-align:middle"><b>{{$Movie['title']}}</b></td>
-                        <td style="width:13%"><a style="text-align:right" href="" class="btn btn-primary btn-sm">Add to List</a></td>
+                        <td width=5%  style="text-align:center; vertical-align:middle"><i style="color: #FFAE00"class="fas fa-star"></i><b>{{$Movie['vote_average']}}</b></td>
+                        <td style="text-align:center; vertical-align:middle;width:13%"><a style="text-align:right" href="{{ route('home.addListMovies', ['id'=>$Movie['id']]) }}" class="btn btn-primary btn-sm">Add to List</a></td>
                     </tr>
                 @endforeach
             </tbody>
@@ -38,10 +39,11 @@
         </div>
 
         <div style="float: left; width: 50%">
-        <table class="table table-strip table-bordered table-hover">
+        <table class="table table-strip table-bordered table-hover" style="background: #F4F6F9">
             <thead>
                 <th>Poster</th>
-                <th>Title</th>
+                <th>Serie Title</th>
+                <th>Score</th>
                 <th></th>
             </thead>
 
@@ -50,7 +52,8 @@
                     <tr>
                         <td style="width:5%"><img width = 95.4px height = 142.8px src="{{'https://image.tmdb.org/t/p/w500/'.$Serie['poster_path']}}" alt="poster"></td>
                         <td style="text-align:center; vertical-align:middle"><b>{{$Serie['original_name']}}</b></td>
-                        <td style="width: 13%">
+                        <td width=5% style="text-align:center; vertical-align:middle"><i style="color: #FFAE00"class="fas fa-star"></i><b>{{$Serie['vote_average']}}</b></td>
+                        <td style="text-align:center; vertical-align:middle;width: 13%">
                             {!! Form::open(['route'=> ['home.addListSeries']]) !!}
 
                             <div hidden class="form-group">
