917b46931356c8dd3922f4b348d49fe4734f390b
Tiago Seben <tiagolbs31@gmail.com>
2020-06-21 22:11:30 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ Route::get('/', function () {
 Route::group(['middleware'=>'auth'], function() {
     Route::group(['prefix'=>'serieslist', 'where'=>['id'=>'[0-9]+']], function() {
         Route::any('',                  ['as'=>'serieslist',                    'uses'=>'seriesListController@index']);
-        Route::get('create',            ['as'=>'serieslist.create',             'uses'=>'seriesListController@create']);
         Route::get('{id}/destroy',      ['as'=>'serieslist.destroy',            'uses'=>'seriesListController@destroy']);
         Route::get('{id}/edit',         ['as'=>'serieslist.edit',               'uses'=>'seriesListController@edit']);
         Route::put('{id}/update',       ['as'=>'serieslist.update',             'uses'=>'seriesListController@update']);
@@ -43,8 +42,17 @@ Route::group(['middleware'=>'auth'], function() {
     });
 
     Route::group(['prefix'=>'home', 'where'=>['id'=>'[0-9]+']], function() {
-        Route::any('',                       ['as'=>'home',                'uses'=>'HomeController@index']);
-        Route::any('addListSeries',              ['as'=>'home.addListSeries',             'uses'=>'HomeController@addListSeries']);
+        Route::any('',                                ['as'=>'home',                                'uses'=>'HomeController@index']);
+        Route::any('addListSeries',                   ['as'=>'home.addListSeries',                  'uses'=>'HomeController@addListSeries']);
+        Route::any('{id}/addListMovies',              ['as'=>'home.addListMovies',                  'uses' => 'HomeController@addListMovies']);
+    });
+
+    Route::group(['prefix'=>'movieslist', 'where'=>['id'=>'[0-9]+']], function() {
+        Route::any('',                           ['as'=>'movieslist',                                'uses'=>'moviesListController@index']);
+        Route::get('{id}/destroy',               ['as'=>'movieslist.destroy',                        'uses'=>'moviesListController@destroy']);
+        Route::get('{id}/changeStatus',          ['as'=>'movieslist.changeStatus',                   'uses'=>'moviesListController@changeStatus']);
+        Route::get('plantowatch',                ['as'=>'movieslist.onlyPlanToWatch',                'uses'=>'moviesListController@onlyPlanToWatch']);
+        Route::get('onlyWatched',                ['as'=>'movieslist.onlyWatched',                    'uses'=>'moviesListController@onlyWatched']);
 
     });
 
@@ -54,6 +62,8 @@ Route::group(['middleware'=>'auth'], function() {
 Route::group(['prefix'=>'user', 'where'=>['id'=>'[0-9]+']], function() {
     Route::get('{name}/profile',            ['as'=>'user.profile',             'uses'=>'UserController@profile']);
     Route::get('{name}/serieslist',            ['as'=>'user.publicList',             'uses'=>'UserController@publicList']);
+    Route::get('{name}/movieslist',            ['as'=>'user.publicMoviesList',             'uses'=>'UserController@publicMoviesList']);
+
 });
 
 
@@ -63,5 +73,4 @@ Route::group(['prefix'=>'user', 'where'=>['id'=>'[0-9]+']], function() {
 Route::get('series', 'seriesController@index');
 Route::get('series/create', 'seriesController@create');
 Route::post('series/store', 'seriesController@store');
-Route::get('movieslist', 'moviesListController@index');
 Auth::routes();
