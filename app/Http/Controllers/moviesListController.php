318c04a17f1672303ea8fe01979b46888b77559b
Tiago Seben <tiagolbs31@gmail.com>
2020-06-23 17:57:38 -0300

    Update moviesListController.php

--- app/Http/Controllers/moviesListController.php
+++ app/Http/Controllers/moviesListController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Movies;
 use App\listMovie;
 use Auth;
+use Illuminate\Support\Facades\Http;
+
 
 class moviesListController extends Controller
 {
@@ -15,14 +17,14 @@ class moviesListController extends Controller
             $movies = listMovie::join('movies', 'movies.idMovie', '=', 'list_movies.idMovie')
                                 ->where('list_movies.idUser', auth()->user()->id)
                                 ->select('list_movies.status as status', 'list_movies.idListMovie as idListMovies','movies.notaMovie as score','movies.poster as poster', 'movies.nome as nomeMovie', 'list_movies.created_at as createDate', 'list_movies.updated_at as lastUpdate')
-                                ->orderBy('status', 'desc')->orderBy('nomeMovie')->paginate(8);
+                                ->orderBy('status')->orderBy('nomeMovie')->paginate(8);
         }
         else{
             $movies = listMovie::join('movies', 'movies.idMovie', '=', 'list_movies.idMovie')
                                 ->where('list_movies.idUser', auth()->user()->id)
                                 ->select('list_movies.status as status', 'list_movies.idListMovie as idListMovies','movies.notaMovie as score','movies.poster as poster', 'movies.nome as nomeMovie', 'list_movies.created_at as createDate', 'list_movies.updated_at as lastUpdate')
                                 ->where('movies.nome', 'like', '%'.$filtragem.'%')
-                                ->orderBy('status', 'desc')->orderBy('nomeMovie')->paginate(8)->setpath('movies?desc_filtro=',$filtragem);
+                                ->orderBy('status')->orderBy('nomeMovie')->paginate(8)->setpath('movies?desc_filtro=',$filtragem);
         }
 
         return view('movieslist.index', ['movies' => $movies]);
@@ -51,7 +53,7 @@ class moviesListController extends Controller
                                 ->where('list_movies.idUser', auth()->user()->id)
                                 ->select('list_movies.status as status', 'list_movies.idListMovie as idListMovies','movies.notaMovie as score','movies.poster as poster', 'movies.nome as nomeMovie', 'list_movies.created_at as createDate', 'list_movies.updated_at as lastUpdate')
                                 ->where('status', '=', 'Plan to Watch')
-                                ->orderBy('status', 'desc')->orderBy('nomeMovie')->paginate(8);
+                                ->orderBy('status')->orderBy('nomeMovie')->paginate(8);
         
         return view('movieslist.index', ['movies' => $movies]);
     }
@@ -61,8 +63,14 @@ class moviesListController extends Controller
                                 ->where('list_movies.idUser', auth()->user()->id)
                                 ->select('list_movies.status as status', 'list_movies.idListMovie as idListMovies','movies.notaMovie as score','movies.poster as poster', 'movies.nome as nomeMovie', 'list_movies.created_at as createDate', 'list_movies.updated_at as lastUpdate')
                                 ->where('status', '=', 'Watched')
-                                ->orderBy('status', 'desc')->orderBy('nomeMovie')->paginate(8);
+                                ->orderBy('status')->orderBy('nomeMovie')->paginate(8);
         
         return view('movieslist.index', ['movies' => $movies]);
+    }
+
+    public function description(Request $request){
+        $movie = Http::get('https://api.themoviedb.org/3/movie/'.\Crypt::decrypt($request->get('id')).'?api_key='.config('services.tmdb.token').'&append_to_response=videos,credits')->json();
+
+        return view('movieslist.description', compact('movie'));
     }
 }
